53340c21b8282ac93cf2b11f5e486272b16ac136
MariamDabous <mariam.dabous@hotmail.com>
2022-09-12 14:01:13 +0300

    final

---
+++ loop/for_loop_basic1.py
@@ -0,0 +1,29 @@
+for x in range(0,151,1):
+    print(x)
+
+for y in range(5,1001,5):
+    print(y)
+
+for i in range(1,101,1):
+    if(i%10==0):
+        print("Coding Dojo")
+    elif(i%5==0):
+        print("Coding")
+    else:
+        print(i)
+
+sum=0
+for j in range(1,500000,2):
+    sum=sum+j
+print(sum)
+
+for f in range(2018,0,-4):
+    print(f)
+
+lowNum=10
+highNum=100
+mult=4
+for s in range(lowNum,highNum):
+    if(s%mult==0):
+        print(s)
+
