91420399bf412472795aeb2d89024c9f3172a717
MariamDabous <mariam.dabous@hotmail.com>
2022-09-12 13:32:36 +0300

    initial

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+your_name= "Mariam"
+print("Hello",your_name)
+
+print("Hello"+" "+ your_name)
+
+fave_num=5
+print("Hello",fave_num)
+print("Hello"+ " "+str(fave_num))
+
+food_one ="Apple"
+food_two= "Tomatoes"
+print("I love to eat {} and {}".format(food_one,food_two))
+print(f"I love to eat {food_one} and {food_two}")
